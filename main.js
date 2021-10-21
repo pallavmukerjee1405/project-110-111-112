5815479dd8e8357d558c3ca25ab7dcefbfd929d0
pallavmukerjee1405 <81314117+pallavmukerjee1405@users.noreply.github.com>
2021-10-21 20:17:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -33,3 +33,40 @@ function speak(){
    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
    synth.speak(utterThis);
 }
+
+function check(){
+   img=document.getElementById('captured_image');
+   classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+   if(error){
+      console.error(error);
+   }
+   else{
+      console.log(results);
+      document.getElementById("result_gesture_name").innerHTML=results[0].label;
+      document.getElementById("result_gesture_name2").innerHTML=results[1].label;
+      prediction_1=results[0].label;
+      prediction_2=results[1].label;
+      speak();
+      if(results[0].label=="Best"){
+         document.getElementById("update_gesture").innerHTML="&#128077;"
+      }
+      if(results[0].label=="Amazing"){
+         document.getElementById("update_gesture").innerHTML="&#128076;"
+      }
+      if(results[0].label=="Victory"){
+         document.getElementById("update_gesture").innerHTML="&#9996;"
+      }
+      if(results[1].label=="Best"){
+         document.getElementById("update_gesture2").innerHTML="&#128077;"
+      }
+      if(results[1].label=="Amazing"){
+         document.getElementById("update_gesture2").innerHTML="&#128076;"
+      }
+      if(results[1].label=="Victory"){
+         document.getElementById("update_gesture2").innerHTML="&#9996;"
+      }
+   }
+}
